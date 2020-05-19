33bfa68b7db734bce9405e4b6364dc1c61234efa
ElizaFlores11 <60673917+ElizaFlores11@users.noreply.github.com>
2020-05-19 13:06:15 -0500

    Cambio de Testeo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,42 @@
-const cipher = {
-  // ...
-};
+const cipher = {encode, decode};
+
+function encode (offset, string){
+   let msg = '';
+    for (let i = 0; i < string.length; i++) {
+        let caracteres = string[i];
+        if (caracteres.match(/[a-z]/i)) {
+            if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+                let frase = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+                     msg+= String.fromCharCode(frase);
+            } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+                let frase = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
+                 msg+= String.fromCharCode(frase);
+            }
+        } else {
+            msg+= caracteres; 
+        }
+    }
+    return msg;
+
+}
+
+function decode (offset, string){
+    let msgdecode = "";
+    for (let i = 0; i < string.length; i++) {
+        let caracterdecode = string[i];
+        if (caracterdecode.match(/[a-z]/i)) {
+            if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+                let frase = (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;
+                msgdecode += String.fromCharCode(frase);
+            } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+                let frase = ((string.charCodeAt(i) - 97 - parseInt(offset) + 52) % 26) + 97;
+                msgdecode += String.fromCharCode(frase);
+            }
+         } else {
+            msgdecode += caracterdecode;
+         }
+    }
+    return msgdecode;
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const pantallacifrado = document.getElementById("pantalla-encode");
+const pantalladecifrado= document.getElementById("pantalla-decode");
+const regresar = document.getElementById("regresar");
+const regresar2= document.getElementById("regresar2");
+const informacion= document.getElementById("info");
+
+/*Mostrar datos para cifrar */
+pantallacifrado.addEventListener("click", function() {
+  document.getElementById("mensaje-encode").style.display = 'block'; 
+  document.getElementById("login").style.display = 'none'; 
+});
+/*Mostrar datos para decifrar */
+pantalladecifrado.addEventListener("click", function() {
+  document.getElementById("mensaje-decode").style.display = 'block'; 
+  document.getElementById("login").style.display = 'none'; 
+});
+
+/*Regresa a los botones de inicio desde cifrado */
+regresar.addEventListener("click", function() {
+  document.getElementById("mensaje-encode").style.display = 'none'; 
+  document.getElementById("mensaje-decode").style.display = 'none'; 
+  document.getElementById("login").style.display = 'block'; 
+});
+
+/*Regresa a los botones de inicio desde cifrado */
+regresar2.addEventListener("click", function() {
+  document.getElementById("mensaje-encode").style.display = 'none'; 
+  document.getElementById("mensaje-decode").style.display = 'none'; 
+  document.getElementById("login").style.display = 'block'; 
+});
+/*rescatando el valor del texto a codificar */
+const btnTextEncode = document.getElementById("btn_encode");
+  btnTextEncode.addEventListener("click", function() {
+  let textEncode = document.getElementById("box-msg-encode").value;
+  //textEncode = textEncode.toUpperCase();
+  let number_offset = document.getElementById("numero-encode").value;
+  document.getElementById("resultado-cifrado").innerHTML = cipher.encode(number_offset, textEncode) ;
+});
+
+/*rescatando el valor del texto a decodificar*/
+const btnTextDecode = document.getElementById("btn_decode");
+  btnTextDecode.addEventListener("click", function() {
+  let textDecode = document.getElementById("box-msg-decode").value;
+  //textDecode = textDecode.toUpperCase();
+  let number_offset = document.getElementById("numero-decode").value;
+  document.getElementById("respuesta-decode").innerHTML = cipher.decode(number_offset, textDecode);
+});
+
+informacion.addEventListener("click", function() {
+  alert("No puede utilizar la letra 'Ñ'");  
+});
